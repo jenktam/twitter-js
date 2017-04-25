73acd6cf35f81e4610707e3cf1d8d72df32a68cb
Jen Tam <jennifer.k.tam@gmail.com>
2017-04-25 07:22:19 -0700

    Added morgan middleware

--- app.js
+++ app.js
@@ -4,16 +4,34 @@ const express = require('express');
 const chalk = require('chalk');
 const router = express.Router();
 const app = express();
+const volleyball = require('volleyball');
+const morgan = require('morgan');
 
-app.use('/', function(request, response, next) {
-  // GOAL --> display: GET / 200
-  //                   http-verb url status
-  let status = 200;
-  response.status = 200;
-  let text = request.method + ' ' + request.url;
-  let output = chalk.blue(text + ' ' + status);
-  console.log(output);
-  next();
+
+// Longer method to log middleware responses
+// app.use('/', function(request, response, next) {
+//   // GOAL --> display: GET /news, GET / 200
+//   //                   http-verb url status
+//   // let status = 200;
+//   // response.status = 200;
+//   // response.send()
+//   // let text = request.method + ' ' + request.url;
+//   // let output = chalk.blue(text + ' ' + status);
+//   // console.log(output);
+//   console.log(request.method + ' ' + request.url + ' ' + response.statusCode);
+//   next();
+// })
+
+// Quicker method to log middleware responses using morgan npm
+app.use(morgan('dev'));
+
+
+// var cats = [];
+
+// Error-handling middleware
+app.use(function(error, requeust, response, next){
+  console.log('Something went wrong', error);
+  response.send('Something went wrong: ' + error.message);
 })
 
 app.get('/', function(request, response) {
@@ -22,7 +40,14 @@ app.get('/', function(request, response) {
 
 app.get('/news', function(request, response) {
   response.send('News ...');
-})
+});
+
+// app.post('/cats', function(request, response) {
+//   response.send('This is the POST /cats page');
+//   var cat = Math.random() > 0.5 ? cat = '😺' : cat = '😿';
+//   cats.push(cat);
+//   response.json(cats);
+// });
 
 app.post('/', function(request, response) {
   response.send('Doing a post...');
