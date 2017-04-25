6b226f2ebf7ce488b3fc72b33354b3fe43de67c3
Jen Tam <jennifer.k.tam@gmail.com>
2017-04-25 08:45:20 -0700

    Added nunjucks templating engine and configured with express

--- app.js
+++ app.js
@@ -1,53 +1,43 @@
 'use strict';
 
+// REQUIRING MODULES
 const express = require('express');
 const chalk = require('chalk');
 const router = express.Router();
 const app = express();
 const volleyball = require('volleyball');
 const morgan = require('morgan');
+const nunjucks = require('nunjucks');
 
+// CONFIGURING APP
+app.set('view engine', 'html'); // have res.render work with html files
+app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
+nunjucks.configure('views', {noCache: true}); // point nunjucks to the proper directory for templates
+// nunjucks.render('index.html', locals, function (err, output) {
+//     console.log(output);
+// });
 
-// Longer method to log middleware responses
-// app.use('/', function(request, response, next) {
-//   // GOAL --> display: GET /news, GET / 200
-//   //                   http-verb url status
-//   // let status = 200;
-//   // response.status = 200;
-//   // response.send()
-//   // let text = request.method + ' ' + request.url;
-//   // let output = chalk.blue(text + ' ' + status);
-//   // console.log(output);
-//   console.log(request.method + ' ' + request.url + ' ' + response.statusCode);
-//   next();
-// })
+// ERROR-HANDLING MIDDLEWARE
+app.use(morgan('dev')); // Quicker method to log middleware responses using morgan npm
 
-// Quicker method to log middleware responses using morgan npm
-app.use(morgan('dev'));
-
-
-// var cats = [];
-
-// Error-handling middleware
-app.use(function(error, requeust, response, next){
+app.use(function(error, request, response, next){
   console.log('Something went wrong', error);
   response.send('Something went wrong: ' + error.message);
 })
 
+// ROUTERS
 app.get('/', function(request, response) {
-  response.send('Hey, what is up..................');
+  // response.send('Hey, what is up..................');
+  const people = [{name: 'Full', age: 10}, {name: 'Stacker', age: 5}, {name: 'Son', age: 50}];
+  var fn = function(){
+    return "hey";
+  };
+  response.render( 'index', {title: 'Hall of Fame', people: people, fn: fn} );
 })
 
 app.get('/news', function(request, response) {
   response.send('News ...');
 });
-
-// app.post('/cats', function(request, response) {
-//   response.send('This is the POST /cats page');
-//   var cat = Math.random() > 0.5 ? cat = '😺' : cat = '😿';
-//   cats.push(cat);
-//   response.json(cats);
-// });
 
 app.post('/', function(request, response) {
   response.send('Doing a post...');
@@ -56,3 +46,19 @@ app.post('/', function(request, response) {
 app.listen(3000, function() {
     console.log('Listening to port 3000...');
 })
+
+
+
+
+
+// // Added nunjucks Templating Engine
+// // in some file that is in the root directory of our application... how about app.js?
+// var locals = {
+//     title: 'An Example',
+//     people: [
+//         { name: 'Gandalf'},
+//         { name: 'Frodo' },
+//         { name: 'Hermione'}
+//     ]
+// };
+
